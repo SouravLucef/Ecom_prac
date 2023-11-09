56bc1d7d0c907bf0b9bc0f9c3c34fb340810ba3b
Lucefer256 <sm30122000@gmail.com>
2023-11-10 00:51:21 +0530

    changes

--- app.js
+++ app.js
@@ -2,10 +2,22 @@ const express = require("express");
 
 const app = express();
 
+const customMiddleware = (req,res,next) =>{
+    console.log("Welcome to my middleware");
+    next();
+};
+
+app.use(customMiddleware);
+
 app.get("/",(req,res) =>{
     res.send("App is running");
 
 });
+
+
+app.post("/creat_user",(req,res) =>{
+    console.log(req);
+})
 
 app.listen(5000,()=>{
     console.log("Listening to port 5000");
