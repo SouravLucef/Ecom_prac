020545df884a6e80c0cae4e4d271011c7a140637
Lucefer256 <sm30122000@gmail.com>
2023-11-12 01:20:55 +0530

    added post

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json()); //conver the req into json file so that it can understand
+
 const customMiddleware = (req,res,next) =>{
     console.log("Welcome to my middleware");
     next();
@@ -16,7 +18,10 @@ app.get("/",(req,res) =>{
 
 
 app.post("/creat_user",(req,res) =>{
-    console.log(req);
+    console.log(req.body); // we write here req.body as we give our date in body as a json file
+    
+    // res.send("created your user "+ req.body.name+" whose age is "+req.body.age);
+    res.send(`User name is ${req.body.name} and his age is ${req.body.age}`)
 })
 
 app.get("/users",(req,res) =>{
@@ -24,13 +29,15 @@ app.get("/users",(req,res) =>{
     // res.send("Here are your users" + users);
     res.send(
         {
-         "users":  users, 
+         users:  users, 
         });
 })
 
 app.delete("/delete",(req,res)=> {
+    
 
 })
+app.patch("/patch")
 
 app.listen(5000,()=>{
     console.log("Listening to port 5000");
